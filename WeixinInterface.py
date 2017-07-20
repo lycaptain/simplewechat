fc84adc4093f3a77553247bec2875878cce29258
Nobody <nobody@yq150.onlinetools>
2017-07-20 11:06:37 +0800

    git commit by online edit file 1/WeixinInterface.py

---
+++ WeixinInterface.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import hashlib
+import web
+import lxml
+import time
+import os
+import urllib2, json
+from lxml import etree
+
+class WeixinInterface:
+    
+    def _init_(self):
+        self.app_root = os.path.dirname(_file_)
+        self.templates_root = os.path.join(self.app_root, 'templates')
+        self.render = web.template.render(self.templates_root)
+        
+    def GET(self):
+        #获取输入参数
+        data = web.input()
+        signature = data.signature
+        timestamp = data.timestamp
+        nonce = data.nonce
+        echostr = data.echostr
+        
+        #自己的token
+        token = "41512241"
+        
+        #字典序排序
+        list = [token, timestamp, nonce]
+        list.sort()
+        sha1 = hashlib.sha1()
+        map(sha1.uodate, list)
+        hashcode = sha1.hexdigest()
+        #sha1加密算法
+        
+        #如果是来自微信服务端的请求则回复echostr
+        if hashcode == signature:
+            return echostr
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
