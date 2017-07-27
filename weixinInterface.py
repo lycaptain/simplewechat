079e3425b72dfde0999553acd4f837914dd538b8
Nobody <nobody@yq150.onlinetools>
2017-07-27 11:34:58 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -13,7 +13,6 @@ class WeixinInterface:
         self.app_root = os.path.dirname(__file__)
         self.templates_root = os.path.join(self.app_root, 'templates')
         self.render = web.template.render(self.templates_root)
-
    
 	def GET(self):
         #获取输入参数
