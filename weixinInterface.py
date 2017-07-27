5673374a162e38196e0c1d6e7436ad22c41cba43
Nobody <nobody@yq150.onlinetools>
2017-07-27 11:30:53 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -35,7 +35,26 @@ class WeixinInterface:
         if hashcode == signature:
             return echostr
 
-	def POST(self):        
+	def POST(self):
+        #获取输入参数
+        data = web.input()
+        signature=data.signature
+        timestamp=data.timestamp
+        nonce=data.nonce
+        echostr=data.echostr
+        #自己的token
+        token="anmmd" #这里改写你在微信公众平台里输入的token
+        #字典序排序
+        list=[token,timestamp,nonce]
+        list.sort()
+        sha1=hashlib.sha1()
+        map(sha1.update,list)
+        hashcode=sha1.hexdigest()
+        #sha1加密算法        
+
+        #如果是来自微信的请求，则回复echostr
+        if hashcode == signature:
+            return echostr        
    
             
             
