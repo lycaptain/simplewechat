773162be7aee5559b839837e5f33ed69fa7f5761
Nobody <nobody@yq150.onlinetools>
2017-07-27 11:22:24 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -47,3 +47,9 @@ class WeixinInterface:
                 return self.render.reply_text(fromUser, toUser, int(time.time()), "随便看看？（对不起我功能有限QAQ）")
             else:
                 return self.render.reply_text(fromUser, toUser, int(time.time()), "哎呀出错了 输入个help看看如何正确的调戏我？")
+            
+            
+            
+            
+            
+            
