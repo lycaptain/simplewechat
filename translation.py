61130d58a6e3b5e759447e653ea911cdd07ea3b8
Nobody <nobody@yq150.onlinetools>
2017-08-06 13:40:48 +0800

    git commit by online edit file 1/translation.py

--- translation.py
+++ translation.py
@@ -8,7 +8,7 @@ import urllib2,json
 from lxml import etree
 
 class Translation:
-	def	youdao(word)
+	def	youdao(word):
         qword = urllib2.quote(word)	#屏蔽特殊字符，为了得到规范url
         baseurl = r'http://fanyi.youdao.com/openapi.do?keyfrom=yourAppName&key=yourAppKey&type=data&doctype=json&version=1.1&q='
         url = baseurl + qword
