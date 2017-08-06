21a93c49d3c5bef58a40532563bfc600e1aa5663
Nobody <nobody@yq150.onlinetools>
2017-08-06 20:31:18 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -74,8 +74,7 @@ class WeixinInterface:
             #if type(content).__name__ == "unicode":
             #	content = content.encode('UTF-8')
             N = WeixinInterface()
-            Nword = "HI"
-            N.youdaoz(Nword)        
+            N.youdaoz(content)        
             return self.render.reply_text(fromUser,toUser,int(time.time()),"HI")
         
            
