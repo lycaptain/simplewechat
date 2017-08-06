774d24a5f993c459dac22e8e921972ed33d13771
Nobody <nobody@yq150.onlinetools>
2017-08-06 20:34:54 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -73,7 +73,7 @@ class WeixinInterface:
             #	content = content.encode('UTF-8')
             N = WeixinInterface()
             #youdaoz(content)
-            N.youdaoz()        
+            youdaoz()        
             return self.render.reply_text(fromUser,toUser,int(time.time()),"HI")
         
            
