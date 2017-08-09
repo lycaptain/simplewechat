b0079a64538d3273eec0f85d230dbaee27a0d1a7
Nobody <nobody@yq150.onlinetools>
2017-08-09 17:42:30 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -10,9 +10,6 @@ from lxml import etree
 
 class WeixinInterface:
     
-    def youdao(word):
-    	return word
-    
     def __init__(self):
         self.app_root = os.path.dirname(__file__)
         self.templates_root = os.path.join(self.app_root, 'templates')
