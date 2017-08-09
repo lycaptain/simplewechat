e47165565189d7a9a3db74366de9a6f8b23a1e9f
Nobody <nobody@yq150.onlinetools>
2017-08-09 11:26:45 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -14,11 +14,9 @@ class WeixinInterface:
         self.app_root = os.path.dirname(__file__)
         self.templates_root = os.path.join(self.app_root, 'templates')
         self.render = web.template.render(self.templates_root)
-        
-        
+               
    	def youdao(word):
-    	return word
-        
+    	return word        
 
     def GET(self):
         #获取输入参数
