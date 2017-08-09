81813e07f685ac96b9e8089c930fa24b63511bdb
Nobody <nobody@yq150.onlinetools>
2017-08-09 18:00:38 +0800

    git commit by online edit file 1/cxkd.py

--- cxkd.py
+++ cxkd.py
@@ -1,7 +1,7 @@
 import urllib2
-
 def detect_com(postid):
     r = urllib2.urlopen('http://www.kuaidi100.com/autonumber/autoComNum?text='+postid)
     h = r.read()
     k = eval(h)
+    kuaiditpye = k["auto"][0]['comCode']
     return kuaiditpye
