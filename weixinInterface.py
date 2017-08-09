63a8cd00c59e011947d791c479055579c4281303
Nobody <nobody@yq150.onlinetools>
2017-08-09 11:27:58 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -47,7 +47,7 @@ class WeixinInterface:
         toUser = xml.find("ToUserName").text
         if msgType == 'text':
             content = xml.find("Content").text
-            #Nword = youdao(content)
+            Nword = youdao(content)
             #if content == 'help':
                # return self.render.reply_text(fromUser, toUser, int(time.time()), "随便看看？（对不起我功能有限QAQ）")
            # else:
