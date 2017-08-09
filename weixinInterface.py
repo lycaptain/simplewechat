03d9ec6f62ab13093f87500d6a8e185f56056ca9
Nobody <nobody@yq150.onlinetools>
2017-08-09 18:02:16 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -38,21 +38,22 @@ class WeixinInterface:
             return echostr
 	
         
-    def POST(self):
-        str_xml = web.data() #获得post来的数据
-        xml = etree.fromstring(str_xml)#进行XML解析
-        msgType = xml.find("MsgType").text
-        fromUser = xml.find("FromUserName").text
-        toUser = xml.find("ToUserName").text
+    def POST(self): 
+        str_xml = web.data() #获得post来的数据 
+        xml = etree.fromstring(str_xml)#进行XML解析 
+        msgType=xml.find("MsgType").text 
+        fromUser=xml.find("FromUserName").text 
+        toUser=xml.find("ToUserName").text 
         if msgType == 'text':
-            content = xml.find("Content").text
-            
-            if content == 'help':
-                return self.render.reply_text(fromUser, toUser, int(time.time()), "随便看看？（对不起我功能有限QAQ）")
-            else:
-                return self.render.reply_text(fromUser, toUser, int(time.time()), content)
-			
-        	#return self.render.reply_text(fromUser, toUser, int(time.time()), Nword)
+            content=xml.find("Content").text
+            if content[0:2] == u"快递":
+                post = str(content[2:])
+                kuaidi = cxkd.detect_com(post)
+                return self.render.reply_text(fromUser,toUser,int(time.time()), kuaidi)
+        elif msgType == 'image':
+            pass
+        else:
+            pass
         
         
 
