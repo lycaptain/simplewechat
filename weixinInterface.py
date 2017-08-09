319232a2fdc1fab645e7e3d4496f9f24d583c376
Nobody <nobody@yq150.onlinetools>
2017-08-09 17:42:05 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -41,7 +41,6 @@ class WeixinInterface:
 	
         
     def POST(self):
-      	youdao("HI")
         str_xml = web.data() #获得post来的数据
         xml = etree.fromstring(str_xml)#进行XML解析
         msgType = xml.find("MsgType").text
