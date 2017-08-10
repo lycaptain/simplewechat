677f781ebf546d63b2a539e752daf8931acdcb12
Nobody <nobody@yq150.onlinetools>
2017-08-10 16:02:16 +0800

    git commit by online edit file 1/Simsimi.py

--- Simsimi.py
+++ Simsimi.py
@@ -7,5 +7,5 @@ def simsimi(ask):
     baseurl = r'http://sandbox.api.simsimi.com/request.p?key=45afcfa5-395f-4676-836d-0a61af189161&lc=en&ft=1.0&text='
     url = baseurl + qask
     resp = urllib.request.urlopen(url)
-    respjson = json.loads(resp.read().decode())
+    respjson = json.loads(resp.read())
     return respjson
