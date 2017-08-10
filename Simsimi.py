3775403fea6e713e1d4419d166efb7fe87faef34
Nobody <nobody@yq150.onlinetools>
2017-08-10 16:51:39 +0800

    git commit by online edit file 1/Simsimi.py

--- Simsimi.py
+++ Simsimi.py
@@ -2,13 +2,6 @@
 import urllib2
 import json
 def simsimi(ask):
-    #ask = ask.encode('UTF-8')
-    #qask = urllib2.quote(ask)
-    #baseurl = r'http://sandbox.api.simsimi.com/request.p?key=45afcfa5-395f-4676-836d-0a61af189161&lc=en&ft=1.0&text='
-    #url = baseurl + qask
-    #resp = urllib2.urlopen(url)
-    #respjson = json.loads(resp.read())
-    #return respjson
 	ask = ask.encode('UTF-8')
     enask = urllib2.quote(ask)
     send_headers = {
