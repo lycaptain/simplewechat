1baeaef868ba25e3b02645486b8be6504ae86d69
Nobody <nobody@yq150.onlinetools>
2017-08-10 15:08:44 +0800

    git commit by online edit file 1/Simsimi.py

--- Simsimi.py
+++ Simsimi.py
@@ -5,7 +5,7 @@ def simsimi(ask):
     ask = ask.encode('UTF-8')
     qask = urllib2.quote(ask)
     baseurl = r'http://www.simsimi.com/func/req?msg='
-    url = baseurl + qask + ''&lc=ch&ft=0.0'
+    url = baseurl + qask + '&lc=ch&ft=0.0'
     resp = urllib2.openurl(url)
     respjson = json.loads(resp.read())
     return respjson
