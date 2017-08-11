f3f46ace746e5dfd677081722d646e42128b7c62
Nobody <nobody@yq150.onlinetools>
2017-08-11 16:40:11 +0800

    git commit by online edit file 1/DBmodel.py

--- DBmodel.py
+++ DBmodel.py
@@ -5,14 +5,14 @@ import sae.const
 
 class dbOperation:
     
-	db = web.database(
-         dbn='mysql',
-         port=int(sae.const.MYSQL_PORT),
-         host=sae.const.MYSQL_HOST,
-         db=sae.const.MYSQL_DB,
-         user=sae.const.MYSQL_USER,
-         passwd=sae.const.MYSQL_PASS,       
-	)
+    db = web.database(
+        dbn='mysql', 
+        port=int(sae.const.MYSQL_PORT), 
+        host=sae.const.MYSQL_HOST, 
+        db=sae.const.MYSQL_DB, 
+        user=sae.const.MYSQL_USER, 
+        pw=sae.const.MYSQL_PASS
+    )
     
 	def addfk(username, fktime, fkcontent):      	
    		return db.insert('fk', user=username, time=fktime, fk_content=fkcontent)
