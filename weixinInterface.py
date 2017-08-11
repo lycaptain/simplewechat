16e88f4bfe1077ae891e5cbdff86c60a0ac3a2a9
Nobody <nobody@yq150.onlinetools>
2017-08-11 15:22:38 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -10,8 +10,6 @@ import cxkd
 import Simsimi
 import translation
 import pylibmc
-#import DBmodel
-#from DBmodel import dbOperation
 from lxml import etree
 
 class WeixinInterface:
@@ -54,10 +52,6 @@ class WeixinInterface:
         
         if msgType == 'text':
             content=xml.find("Content").text
-            #if content.startswith('fk'):
-        		#fktime = time.strftime('%Y-%m-%d %H:%M',time.localtime())        
-        		#dbOperation.addfk(fromUser,fktime,content[3:].encode('utf-8'))        
-        		#return self.render.reply_text(fromUser,toUser,int(time.time()),u'感谢您的反馈')
             if content[0:2] == u"快递":
                 post = str(content[2:])
                 kuaidi = cxkd.detect_com(post)
