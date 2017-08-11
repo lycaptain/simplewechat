748027461f65578d3a1633e3352d551c0ddeb2d0
Nobody <nobody@yq150.onlinetools>
2017-08-11 13:55:58 +0800

    git commit by online edit file 1/DBmodel.py

--- DBmodel.py
+++ DBmodel.py
@@ -13,8 +13,8 @@ class dbOperation:
         db=sae.const.MYSQL_DB
    		)
     
-    def addfk(username, fktime, fkcontent):
-        return db.insert('fk', user=username, time=fktime, fk_content=fkcontent)
+def addfk(username, fktime, fkcontent):
+   	return db.insert('fk', user=username, time=fktime, fk_content=fkcontent)
 
 
 	#def get_fkcontent():
