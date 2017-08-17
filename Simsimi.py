6a13f3222ad763f907127373b50ce5b02d4548a8
Nobody <nobody@yq150.onlinetools>
2017-08-17 14:37:31 +0800

    git commit by online edit file 1/Simsimi.py

--- Simsimi.py
+++ Simsimi.py
@@ -5,7 +5,6 @@ def simsimi(ask):
     ask = ask.encode('UTF-8')
     enask = urllib2.quote(ask)
     send_headers = {
-
     'Cookie':'dotcom_session_key=s%3A8W3ACqIiGX5WMBNLDEWj1OtwYcmduVlS.E4EHBpJgnHeqz7KFp5MdUlIHUggYblzDGI3gZG2e8Ck; normalProb=4; lc=ch; currentChatCnt=3; _ga=GA1.2.1265999218.1502948173; _gid=GA1.2.1301977380.1502948173; _gat=1'
     }
     baseurl = r'http://www.simsimi.com/getRealtimeReq?lc=ch&ft=1&normalProb=4&reqText='
