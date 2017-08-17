2cece90398ad29cd152c23ab2590f8b8d4209075
Nobody <nobody@yq150.onlinetools>
2017-08-17 15:33:29 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -99,7 +99,7 @@ class WeixinInterface:
             	replayText = u'welcome'
             	return self.render.reply_text(fromUser,toUser,int(time.time()), replayText)			
             elif content == 'unsubscribe':
-                replayText = u'再见'
+                replayText = u'bye'
                	return self.render.reply_text(fromUser,toUser,int(time.time()), replayText)
             
             
