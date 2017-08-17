f01f5880cd075fe8a91fc0903c40c3a9471344ec
Nobody <nobody@yq150.onlinetools>
2017-08-17 14:36:35 +0800

    git commit by online edit file 1/Simsimi.py

--- Simsimi.py
+++ Simsimi.py
@@ -3,10 +3,14 @@ import urllib2
 import json
 def simsimi(ask):
     ask = ask.encode('UTF-8')
-    qask = urllib2.quote(ask)
-    #baseurl = r'http://sandbox.api.simsimi.com/request.p?key=45afcfa5-395f-4676-836d-0a61af189161&lc=en&ft=1.0&text='
+    enask = urllib2.quote(ask)
+    send_headers = {
+
+    'Cookie':'dotcom_session_key=s%3A8W3ACqIiGX5WMBNLDEWj1OtwYcmduVlS.E4EHBpJgnHeqz7KFp5MdUlIHUggYblzDGI3gZG2e8Ck; normalProb=4; lc=ch; currentChatCnt=3; _ga=GA1.2.1265999218.1502948173; _gid=GA1.2.1301977380.1502948173; _gat=1'
+    }
     baseurl = r'http://www.simsimi.com/getRealtimeReq?lc=ch&ft=1&normalProb=4&reqText='
-    url = baseurl + qask +u"&status=W&talkCnt=3"
-    resp = urllib2.urlopen(url)
-    respjson = json.loads(resp.read())
-    return respjson
+    url = baseurl + enask + u"&status=W&talkCnt=3"
+    req = urllib2.Request(url,headers=send_headers)
+    resp = urllib2.urlopen(req)
+    reson = json.loads(resp.read())
+    return reson
