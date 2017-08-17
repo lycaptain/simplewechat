f655b70636fe31e864cceb0af059257c258b0d50
Nobody <nobody@yq150.onlinetools>
2017-08-17 15:40:35 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -11,52 +11,53 @@ import Simsimi
 import translation
 import pylibmc
 import DBmodel
+from DBmodel import dbOperation
 from lxml import etree
 os.environ['disable_fetchurl'] = "1"
 
 class WeixinInterface:
     
-	def __init__(self):
-		self.app_root = os.path.dirname(__file__)
-		self.templates_root = os.path.join(self.app_root, 'templates')
-		self.render = web.template.render(self.templates_root)
+    def __init__(self):
+        self.app_root = os.path.dirname(__file__)
+        self.templates_root = os.path.join(self.app_root, 'templates')
+        self.render = web.template.render(self.templates_root)
                      
-	def GET(self):
-		#获取输入参数
-		data = web.input()
-		signature = data.signature
-		timestamp = data.timestamp
-		nonce = data.nonce
-		echostr = data.echostr
-		#自己的token
-		token = "anmmd" #这里改写你在微信公众平台里输入的token
-		#字典序排序
-		list = [token,timestamp,nonce]
-		list.sort()
-		sha1 = hashlib.sha1()
-		map(sha1.update,list)
-		hashcode = sha1.hexdigest()
-		#sha1加密算法        
+    def GET(self):
+        #获取输入参数
+        data = web.input()
+        signature = data.signature
+        timestamp = data.timestamp
+        nonce = data.nonce
+        echostr = data.echostr
+        #自己的token
+        token = "anmmd" #这里改写你在微信公众平台里输入的token
+        #字典序排序
+        list = [token,timestamp,nonce]
+        list.sort()
+        sha1 = hashlib.sha1()
+        map(sha1.update,list)
+        hashcode = sha1.hexdigest()
+        #sha1加密算法        
 
-		#如果是来自微信的请求，则回复echostr
-		if hashcode == signature:
-			return echostr
+        #如果是来自微信的请求，则回复echostr
+        if hashcode == signature:
+            return echostr
 	
         
-	def POST(self): 
-		str_xml = web.data() #获得post来的数据 
-		xml = etree.fromstring(str_xml)#进行XML解析 
-		msgType = xml.find("MsgType").text 
-		fromUser = xml.find("FromUserName").text 
-		toUser = xml.find("ToUserName").text 
-		mc = pylibmc.Client()
+    def POST(self): 
+        str_xml = web.data() #获得post来的数据 
+        xml = etree.fromstring(str_xml)#进行XML解析 
+        msgType = xml.find("MsgType").text 
+        fromUser = xml.find("FromUserName").text 
+        toUser = xml.find("ToUserName").text 
+        mc = pylibmc.Client()
         #初始化一个memcache实例来保存用户的操作
         
-		if msgType == 'text':
-        	content = xml.find("Content").text
-        	if content.startswith('fk'):
-       			fktime = time.strftime('%Y-%m-%d %H:%M',time.localtime())        
-        		ans = DBmodel.addfk(fromUser,fktime,content[3:].encode('utf-8'))  
+        if msgType == 'text':
+            content=xml.find("Content").text
+            if content.startswith('fk'):
+        		fktime = time.strftime('%Y-%m-%d %H:%M',time.localtime())        
+        		ans = dbOperation.addfk(fromUser,fktime,content[3:].encode('utf-8'))  
         		return self.render.reply_text(fromUser,toUser,int(time.time()),u'感谢您的反馈'+ans)
             if content[0:2] == u"快递":
                 post = str(content[2:])
@@ -90,17 +91,17 @@ class WeixinInterface:
             Nword = translation.youdao(content)
             return self.render.reply_text(fromUser,toUser,int(time.time()), Nword)	         
         
-		elif msgType == 'image':
+        elif msgType == 'image':
         	pass
-		
-        elif msgType == 'event':
+       
+    	elif msgType == 'event':
         	content = xml.find("Event").text
         	if content == 'subscribe':
-            	replayText = u'welcome'
+            	replayText = u'欢迎关注本微信'
             	return self.render.reply_text(fromUser,toUser,int(time.time()), replayText)			
             elif content == 'unsubscribe':
-            	replayText = u'bye'
-            	return self.render.reply_text(fromUser,toUser,int(time.time()), replayText)
+                replayText = u'再见'
+               	return self.render.reply_text(fromUser,toUser,int(time.time()), replayText)
             
             
             
