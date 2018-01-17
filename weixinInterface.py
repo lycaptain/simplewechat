37db18d0f1d127ff599abda7a88101963e3c16a7
Nobody <nobody@yq150.onlinetools>
2018-01-17 08:29:10 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -55,7 +55,7 @@ class WeixinInterface:
         
         if msgType == 'text':
             content=xml.find("Content").text
-            if content.startswith('fk'):
+            if content.startswith('oj'):
                 fktime = time.strftime('%Y-%m-%d %H:%M',time.localtime())  
                 return self.render.reply_text(fromUser,toUser,int(time.time()),u'www.1.simplewechat.applinzi.com/oj')
             if content[0:2] == u"快递":
