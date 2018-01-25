6e3ee954a378b45ad783da571a5fb64bbe7eb1fa
Nobody <nobody@yq150.onlinetools>
2018-01-25 16:31:35 +0800

    git commit by online edit file 1/model.py

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import sae.const
 
 db = web.database(
 	db_name = 'mysql'
-    name = sae.const.MYSQL_USER
+    user = sae.const.MYSQL_USER
     pwd = sae.const.MYSQL_PASS
     host = sae.const.MYSQL_HOST
     port = sae.const.MYSQL_PORT
