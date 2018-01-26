6c33b80e6b6d81425981513192d510b73a096d73
Nobody <nobody@yq150.onlinetools>
2018-01-26 12:45:10 +0800

    git commit by online edit add file 1/html_outputer.py

---
+++ html_outputer.py
@@ -0,0 +1,15 @@
+# coding:utf8
+class HtmlOutputer(object):
+
+    def __init__(self):
+        self.datas = []
+
+    def collect_data(self, data):
+        if data is None:
+            return
+        self.datas.append(data)
+
+    def output_html(self):
+
+        for data in self.datas:
+            print data
