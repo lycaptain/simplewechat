d9b4fa37f3c2236060fc11d311da5696eb8d58b8
Nobody <nobody@yq150.onlinetools>
2018-01-26 12:32:09 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -58,7 +58,7 @@ class WeixinInterface:
             content=xml.find("Content").text
             if content.startswith('oj'):
                 return self.render.reply_text(fromUser,toUser,int(time.time()),u'1.simplewechat.applinzi.com/oj')
-            if content.startswitch('update'):
+            if content[0:2] == u"update":
                 return self.render.reply_text(fromUser,toUser,int(time.time()),'ok')
             if content[0:2] == u"快递":
                 post = str(content[2:])
