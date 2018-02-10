8fab617b05ec031a7231af4c4d7ab265ff61018b
Nobody <nobody@yq150.onlinetools>
2018-02-11 01:21:37 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -41,8 +41,8 @@ class WeixinInterface:
         if hashcode == signature:
             return echostr
 	
-	def POST(self):        
-		str_xml = web.data() #获得post来的数据
+    def POST(self):        
+        str_xml = web.data() #获得post来的数据
         xml = etree.fromstring(str_xml)#进行XML解析
         content=xml.find("Content").text#获得用户所输入的内容
         msgType=xml.find("MsgType").text
