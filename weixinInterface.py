a129aa8a8389596317f2f6c07838a90c5328ec00
Nobody <nobody@yq150.onlinetools>
2018-03-05 22:11:12 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -60,8 +60,7 @@ class WeixinInterface:
                 num = 10
             else:
                 num = total_num
-            if OJS[0][0] == 'ZJU':
-                return self.render.reply_text(fromUser,toUser,int(time.time()),u"我现在还在开发中，还没有什么功能，您刚才说的l是："+OJS[0][1])
+            return self.render.reply_morepic(fromUser,toUser,INFOS,num)
         else:
             return self.render.reply_text(fromUser,toUser,int(time.time()),u"我现在还在开发中，还没有什么功能，您刚才说的l是："+content)
             
