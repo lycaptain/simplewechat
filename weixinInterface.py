14864989513f12c4e17d69374840ab7b09b6e7df
Nobody <nobody@yq150.onlinetools>
2018-03-06 15:38:22 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -56,8 +56,8 @@ class WeixinInterface:
             for oj in OJ:
                 OJS.append((oj['oj'], oj['name'], oj['start_time'], oj['link']))
             total_num = len(OJS)
-            if total_num>9:
-                num = 9
+            if total_num>8:
+                num = 8
             else:
                 num = total_num
             return self.render.reply_morepic(fromUser,toUser,OJS,num)
