fb889c127a30685bf6a621df0e37fcc0bc74ab78
Nobody <nobody@yq150.onlinetools>
2018-03-10 14:51:05 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -15,12 +15,12 @@ import re
 os.environ['disable_fetchurl'] = "1"
 
 def token(requset):  
-   	url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s' % (
-    Config.AppID, Config.AppSecret)
-    result = urllib2.urlopen(url).read()
-    Config.access_token = json.loads(result).get('access_token')
-    print 'access_token===%s' % Config.access_token
-    return HttpResponse(result)
+	url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s' % (
+	Config.AppID, Config.AppSecret)
+	result = urllib2.urlopen(url).read()
+	Config.access_token = json.loads(result).get('access_token')
+	print 'access_token===%s' % Config.access_token
+	return HttpResponse(result)
     
 def createMenu(request):  
     url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s" % Config.access_toke
